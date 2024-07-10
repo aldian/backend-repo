e419799fbc5766329cc5b1db749b97d801dfd71b
Aldian Fazrihady <aldian.f@ntucenterprise.sg>
2024-07-10 07:41:03 +0700

    Refactor messages

---
+++ config/messages.ts
@@ -0,0 +1,9 @@
+// Error Messages
+export const MISSING_REQUIRED_FIELDS = 'Missing required fields';
+export const FAILED_TO_UPDATE_DATA = 'Failed to update data';
+export const NO_SUCH_DOCUMENT = 'No such document!';
+export const FAILED_TO_FETCH_DATA = 'Failed to fetch data';
+
+// Success Messages
+export const USER_UPDATED = 'User updated!';
+export const USER_CREATED = 'User created!';
--- controller/userController.ts
+++ controller/userController.ts
@@ -2,14 +2,22 @@ import { Request, Response, NextFunction } from "express";
 import { v4 as uuidv4 } from 'uuid';
 import db from '../firebase';
 import ApiError from '../entities/ApiError';
+import {
+  MISSING_REQUIRED_FIELDS,
+  FAILED_TO_UPDATE_DATA,
+  NO_SUCH_DOCUMENT,
+  FAILED_TO_FETCH_DATA,
+  USER_UPDATED,
+  USER_CREATED,
+} from '../config/messages';
 
 const collectionName = 'USERS';
 
 export const updateUserData = async (req: Request, res: Response, next: NextFunction) => {
-  let { id, ...userData } = req.body; // Destructure `id` and store the rest in `userData`
+  let { id, ...userData } = req.body;
   
   if (!userData.name) {
-    return next(new ApiError(400, 'Missing required fields'));
+    return next(new ApiError(400, MISSING_REQUIRED_FIELDS));
   }
   
   const isUpdate = !!id;
@@ -23,12 +31,12 @@ export const updateUserData = async (req: Request, res: Response, next: NextFunc
     await docRef.set(userData);
   
     if (isUpdate) {
-      res.status(200).json({ message: 'User updated!', id });
+      res.status(200).json({ message: USER_UPDATED, id });
     } else {
-      res.status(201).json({ message: 'User created!', id });
+      res.status(201).json({ message: USER_CREATED, id });
     }
   } catch (error) {
-    next(new ApiError(500, 'Failed to update data'));
+    next(new ApiError(500, FAILED_TO_UPDATE_DATA));
   }
 };
 
@@ -41,7 +49,7 @@ export const fetchUserData = async (req: Request, res: Response, next: NextFunct
       const doc = await docRef.get();
 
       if (!doc.exists) {
-        return next(new ApiError(404, 'No such document!'));
+        return next(new ApiError(404, NO_SUCH_DOCUMENT));
       }
 
       return res.json({ id, ...doc.data() });
@@ -56,6 +64,6 @@ export const fetchUserData = async (req: Request, res: Response, next: NextFunct
       return res.json(usersList);
     }
   } catch (error) {
-    next(new ApiError(500, 'Failed to fetch data'));
+    next(new ApiError(500, FAILED_TO_FETCH_DATA));
   }
 };
