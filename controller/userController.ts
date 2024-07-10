60f20d934a5879e090b68824ca69b6b82e40525c
Aldian Fazrihady <mobile@aldian.net>
2024-07-10 07:11:58 +0700

    Allow to store custom fields (#4)

--- controller/userController.ts
+++ controller/userController.ts
@@ -4,24 +4,22 @@ import db from '../firebase';
 import ApiError from '../entities/ApiError';
 
 export const updateUserData = async (req: Request, res: Response, next: NextFunction) => {
-  let { id, name } = req.body;
-
-  if (!name) {
+  let { id, ...userData } = req.body; // Destructure `id` and store the rest in `userData`
+  
+  if (!userData.name) {
     return next(new ApiError(400, 'Missing required fields'));
   }
-
+  
   const isUpdate = !!id;
   if (!id) {
     id = uuidv4();
   }
-
+  
   try {
     const docRef = db.collection('users').doc(id);
 
-    await docRef.set({
-      name,
-    });
-
+    await docRef.set(userData);
+  
     if (isUpdate) {
       res.status(200).json({ message: 'User updated!', id });
     } else {
